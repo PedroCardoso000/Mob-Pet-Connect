a97303ccf667c155cd6996348a59c59cc9a301ee
PedroCardoso000 <www.pedrolucascardoso@gmail.com>
2025-03-04 23:38:45 -0300

    fix: adjust containers

--- pet-connect/src/pages/user-screen/UserScreen.tsx
+++ pet-connect/src/pages/user-screen/UserScreen.tsx
@@ -1,23 +1,57 @@
 import React from 'react'
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, TouchableOpacity, FlatList } from 'react-native';
 import { userScreenStyles } from './UserScreen';
-
-const UserScreen = () => {
+import SmallText from '../../components/SmallText';
+const profileData = {
+    username: "Kaique Santos",
+    profilePic: require("../../../assets/favicon.png"), 
+    posts: 120,
+    followers: 3400,
+    following: 300,
+    bio: "Desenvolvedor | Apaixonado por React Native 🚀",
+    feed: [
+      { id: "1", image: require("../../../assets/dog.jpg") },
+      { id: "2", image: require("../../../assets/dog.jpg") },
+      { id: "3", image: require("../../../assets/dog.jpg") },
+      { id: "4", image: require("../../../assets/dog.jpg") },
+      { id: "5", image: require("../../../assets/dog.jpg") },
+      { id: "6", image: require("../../../assets/dog.jpg") },
+    ],
+  };
+  
+  const UserProfileScreen = () => {
     return (
-        <>
-        <View style={userScreenStyles.container}>
-            <View style={userScreenStyles.headerStyle}>
-                <Image source={require('../../../assets/favicon.png')}  />
-                <Text>Header</Text>
-                
-            </View>
-            <View style={userScreenStyles.mediumStyles}></View>
-            <View style={userScreenStyles.footerStyles}></View>
+      <View style={userScreenStyles.container}>
+        {/* ✅ HEADER */}
+        <View style={userScreenStyles.header}>
+          <Image source={profileData.profilePic} style={userScreenStyles.profilePic} />
+          <View style={userScreenStyles.userInfo}>
+            <Text style={userScreenStyles.username}>{profileData.username}</Text>
+          </View>
         </View>
-        </>
+  
+  
+        {/* ✅ BIO */}
+        <Text style={userScreenStyles.bio}>{profileData.bio}</Text>
+
+
+        <Text style={userScreenStyles.bio}>{profileData.bio}</Text>
+  
+  
+        {/* ✅ FEED EM GRADE */}
+        <FlatList
+          numColumns={3}
+          data={profileData.feed}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Image source={item.image} style={userScreenStyles.feedImage} />
+          )}
+          style={userScreenStyles.feed}
+        />
+      </View>
     )
 };
 
-export default UserScreen;
+export default UserProfileScreen;
 
 
