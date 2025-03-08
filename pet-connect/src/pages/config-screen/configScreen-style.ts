054ff8d7c486ab5d3926a3753ab9ef6f6f5b7938
PedroCardoso000 <www.pedrolucascardoso@gmail.com>
2025-03-08 17:59:33 -0300

    fix: adjust checkBox and textLong

--- pet-connect/src/pages/config-screen/configScreen-style.ts
+++ pet-connect/src/pages/config-screen/configScreen-style.ts
@@ -6,18 +6,72 @@ const styleConfigScreen = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: "#fff",
-      },
-      header:{
+    },
+    header: {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         gap: 20,
-      },
-      footer: {
+        marginBottom: 10,
+    },
+    footer: {
         width: '80%',
-        display: 'flex',       
-      }
+        display: 'flex',
+    },
+    sexContainer: {
+        backgroundColor: "#F5F5F5", // Cor de fundo igual aos inputs
+        borderRadius: 8,
+        padding: 12,
+        borderWidth: 1, 
+        borderColor: "#E0E0E0",
+        marginTop: 10,
+        position: "relative",
+    },
+    sexLabel: {
+        position: "absolute",
+        top: -10,
+        left: 10,
+        backgroundColor: "#F5F5F5", // Mesma cor do fundo para "sobrepor"
+        borderRadius: 4,
+        paddingHorizontal: 5,
+        
+        opacity: 0.8,
+        fontSize: 14,
+        color: "#333",
+    },
+    checkboxContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginTop: 10,
+        backgroundColor: "#F5F5F5" , 
+        borderRadius: 8,
+        borderColor: "#E0E0E0",
+    },
+    checkboxOption: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    checkbox: {
+        width: 20,
+        height: 20,
+        borderRadius: 4,
+        borderWidth: 2,
+        borderColor: "#499CFA", // Cor azul personalizada
+        justifyContent: "center",
+        alignItems: "center",
+        marginRight: 8,
+    },
+    checkboxSelected: {
+        borderColor: "#499CFA",
+    },
+    checkboxInner: {
+        width: 12,
+        height: 12,
+        opacity: 0.8,
+        backgroundColor: "#499CFA", // Azul quando marcado
+        borderRadius: 2,
+    },
 });
-
 
 export default styleConfigScreen;
--- pet-connect/src/pages/config-screen/configScreen.tsx
+++ pet-connect/src/pages/config-screen/configScreen.tsx
@@ -1,49 +1,100 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import styleConfigScreen from "./configScreen-style";
 import InputComponent from "@/src/components/InputConnect";
-import { useState } from "react";
-
+import React, { useState } from "react";
 
 const ConfigScreen = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [birth, setBirth] = useState<string | undefined>();
+    const [phone, setPhone] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+    const [sex, setSex] = useState<string>(''); // Estado para armazenar "Masculino" ou "Feminino"
 
     return (
-        <>
-            <View style={styleConfigScreen.container}>
-                <View style={styleConfigScreen.header}>
-                    <Image source={require("@/assets/user.png")} />
-                    <Text>Adicionar uma imagem</Text>
-                </View>
-                <View style={styleConfigScreen.footer}>
-                    <InputComponent
-                        placeholder="Nome"
-                        value={name}
-                        icon={require("@/assets/password.png")}
-                        onChangeText={setName}
-                        keyboardType="name-phone-pad"
-                        autoCapitalize="none"
-                    />
-                    <InputComponent
-                        placeholder="Nome"
-                        value={name}
-                        icon={require("@/assets/password.png")}
-                        onChangeText={setName}
-                        keyboardType="name-phone-pad"
-                        autoCapitalize="none"
-                    />
-                    <InputComponent
-                        placeholder="Email"
-                        value={email}
-                        icon={require("@/assets/password.png")}
-                        onChangeText={setEmail}
-                        keyboardType="email-address"
-                        autoCapitalize="none"
-                    />
-                </View>
+        <View style={styleConfigScreen.container}>
+            <View style={styleConfigScreen.header}>
+                <Image source={require("@/assets/user.png")} />
+                <Text>Adicionar uma imagem</Text>
             </View>
-        </>
-    )
+            <View style={styleConfigScreen.footer}>
+                <InputComponent
+                    placeholder="Nome"
+                    value={name}
+                    icon={require("@/assets/password.png")}
+                    onChangeText={setName}
+                    keyboardType="name-phone-pad"
+                    autoCapitalize="none"
+                />
+                <InputComponent
+                    placeholder="Data de Nascimento"
+                    value={birth}
+                    icon={require("@/assets/password.png")}
+                    onChangeText={setBirth}
+                    keyboardType="name-phone-pad"
+                    autoCapitalize="none"
+                />
+
+
+                <View style={styleConfigScreen.sexContainer}>
+                    <Text style={styleConfigScreen.sexLabel}>Sexo</Text>
+
+                    <View style={styleConfigScreen.checkboxContainer}>
+                        {/* Opção Masculino */}
+                        <TouchableOpacity
+                            onPress={() => setSex("Masculino")}
+                            style={styleConfigScreen.checkboxOption}
+                        >
+                            <View style={[styleConfigScreen.checkbox, sex === "Masculino" && styleConfigScreen.checkboxSelected]}>
+                                {sex === "Masculino" && <View style={styleConfigScreen.checkboxInner} />}
+                            </View>
+                            <Text>Masculino</Text>
+                        </TouchableOpacity>
+
+                        {/* Opção Feminino */}
+                        <TouchableOpacity
+                            onPress={() => setSex("Feminino")}
+                            style={styleConfigScreen.checkboxOption}
+                        >
+                            <View style={[styleConfigScreen.checkbox, sex === "Feminino" && styleConfigScreen.checkboxSelected]}>
+                                {sex === "Feminino" && <View style={styleConfigScreen.checkboxInner} />}
+                            </View>
+                            <Text>Feminino</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <InputComponent
+                    placeholder="Email"
+                    value={email}
+                    icon={require("@/assets/password.png")}
+                    onChangeText={setEmail}
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                />
+
+                <InputComponent
+                    placeholder="Telefone"
+                    value={phone}
+                    icon={require("@/assets/password.png")}
+                    onChangeText={setPhone}
+                    keyboardType="phone-pad"
+                    autoCapitalize="none"
+                />
+
+                {/* Criar Compoente */}
+                <InputComponent
+                    placeholder="Telefone"
+                    value={phone}
+                    icon={require("@/assets/password.png")}
+                    onChangeText={setPhone}
+                    keyboardType="phone-pad"
+                    autoCapitalize="none"
+                    style={{ height: 100 }}
+                />
+
+            </View>
+        </View>
+    );
 };
 
 export default ConfigScreen;
