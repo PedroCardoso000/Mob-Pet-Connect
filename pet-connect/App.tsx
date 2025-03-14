2e630bffc62c57fef6182aa8181642d22e29e52f
PedroCardoso000 <www.pedrolucascardoso@gmail.com>
2025-03-13 21:39:34 -0300

    merge 13-03

--- pet-connect/App.tsx
+++ pet-connect/App.tsx
@@ -4,11 +4,11 @@ import LoginScreen from "./src/pages/login/login";
 import React from "react";
 import UserProfileScreen from "./src/pages/user-screen/userProfileScreen";
 import  ConfigScreen  from "./src/pages/config-screen/configScreen";
-
+import {ContactList} from "./src/pages/contactList/index";
 
 export default function App() {
   return (
-    <ConfigScreen/>
+    <ContactList />
   );
 }
 
