7c52f6b42199aa5456a4f1a227ca04fb485e9d7f
P.Cardoso <www.pedrolucascardoso@gmail.com>
2025-04-01 21:28:18 -0300

    fix: auth Login

--- pet-connect/App.tsx
+++ pet-connect/App.tsx
@@ -1,14 +1,18 @@
 import "@/global.css";
 import React from "react";
-import UserProfileScreen from "./src/pages/user-screen/userProfileScreen";
-import  ConfigScreen  from "./src/pages/config-screen/configScreen";
-import {ContactList} from "./src/pages/contactList/index";
-import CreatePet  from "./src/pages/register-pet/create-pet";
-import FriendViewScreen from "./src/pages/friend-view-screen/friend-view-screen";
+import LoginScreen from "./src/pages/login/login";
+import { NavigationContainer } from '@react-navigation/native';
+import Toast from 'react-native-toast-message';
 
 export default function App() {
   return (
-    <CreatePet />
+    <>
+
+      <NavigationContainer>
+        <LoginScreen />
+        <Toast />
+      </NavigationContainer>
+    </>
   );
 }
 
--- pet-connect/src/pages/login/login.tsx
+++ pet-connect/src/pages/login/login.tsx
@@ -3,48 +3,95 @@ import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { loginstyles } from './login-style';
 import InputComponent from '@/src/components/InputConnect';
 import ButtonComponent from '@/src/components/ButtonConnect';
+import { api } from '@/src/api/axios';
+import Toast from 'react-native-toast-message';
+import { getToken } from '@/src/service/tokenService';
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleLogin = () => {
-    console.log('Email:', email);
-    console.log('Password:', password);
+
+  const showToast = (type: 'success' | 'error', text1: string, text2: string) => {
+    Toast.show({
+      type,
+      text1,
+      text2,
+      position: 'top', // ou 'bottom'
+      visibilityTime: 4000,
+      autoHide: true,
+      topOffset: 50,
+
+    });
   };
 
+  async function handleLogin(email: string, password: string) {
+    const token = await getToken();
+    try {
+      if (email && password) {
+        setIsLoading(true);
+   
+        const response = await api.post(
+          '/auth/login',
+          { email, password },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log('Login realizado com sucesso!', response.data);
+        showToast('success', 'Login efetuado com sucesso!', 'Bem-vindo ao app 🚀');
+      } else {
+        console.warn('Preencha todos os campos!', token);
+        showToast('error', 'Erro ao fazer login', 'Preencha todos os campos.');
+      }
+    } catch (error) {
+      console.error('Erro ao fazer login:', token);
+      showToast('error', 'Erro ao fazer login', 'Tente novamente mais tarde');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+
   return (
-    <View style={loginstyles.container}>
-      <View style={loginstyles.headerContainer}>
-        <Image
-          source={require('../../../assets/icone.png')}
-          style={loginstyles.headerIcon}
-        />
-        <Text style={loginstyles.textLogin}>Login</Text>
+    <>
+      <View style={loginstyles.container}>
+        <View style={loginstyles.headerContainer}>
+          <Image
+            source={require('../../../assets/icone.png')}
+            style={loginstyles.headerIcon}
+          />
+          <Text style={loginstyles.textLogin}>Login</Text>
+        </View>
+
+        <View style={loginstyles.footerContainer}>
+
+          <InputComponent
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+
+          <InputComponent placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            styleInput={{ marginBottom: 20 }}
+          />
+
+          <ButtonComponent title="Entrar" onPress={() => handleLogin(email, password)} />
+
+
+        </View>
+        <Text style={loginstyles.textCadastro}>Cadastre-se</Text>
       </View>
 
-      <View style={loginstyles.footerContainer}>
-
-        <InputComponent
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
-
-        <InputComponent placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry 
-          styleInput={{ marginBottom: 20 }}
-        />
-
-        <ButtonComponent title='Entrar' />
-
-      </View>
-      <Text style={loginstyles.textCadastro}>Cadastre-se</Text>
-    </View>
+    </>
   );
 };
 
